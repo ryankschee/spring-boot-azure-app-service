dc08de7113f0bca6631a63ecd277cd7f66e4119f
Ryan Chee <ryan.kschee@gmail.com>
2023-02-25 21:56:01 +0800

    Initial commit

---
+++ src/main/java/com/ryanchee/learning/springbootazureappservice/SpringBootAzureAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ryanchee.learning.springbootazureappservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAzureAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAzureAppServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryanchee/learning/springbootazureappservice/SpringBootAzureAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanchee.learning.springbootazureappservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAzureAppServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
