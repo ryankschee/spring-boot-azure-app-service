33aaf14c913026fdd2c798cd702f6fd1e3c0c963
Ryan Chee <ryan.kschee@gmail.com>
2023-02-25 21:59:31 +0800

    Add 2 GET endpoints for testing

--- src/main/java/com/ryanchee/learning/springbootazureappservice/SpringBootAzureAppServiceApplication.java
+++ src/main/java/com/ryanchee/learning/springbootazureappservice/SpringBootAzureAppServiceApplication.java
@@ -2,12 +2,24 @@ package com.ryanchee.learning.springbootazureappservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootAzureAppServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAzureAppServiceApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello Azure App Service!";
+	}
 }
